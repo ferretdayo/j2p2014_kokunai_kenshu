06cf04b15084d168fef23436ba5aa297a75a1926
ferretdayo <ferretdayo@hotmail.co.jp>
2014-08-06 16:09:50 +0900

    マウスの中心に画像が来るように修正

--- Applet_test/src/Test04.java
+++ Applet_test/src/Test04.java
@@ -3,7 +3,6 @@ import java.awt.Event;
 import java.awt.Graphics;
 import java.awt.Image;
 
-
 public class Test04 extends Applet {
 
 	/**
@@ -13,12 +12,13 @@ public class Test04 extends Applet {
 
 	int x=0,y=0;
 	Image img1;
+	int size = 36;
 	public void init(){
 		img1 = getImage(getDocumentBase(), "img/0.png");
 		repaint();
 	}
 	public void paint(Graphics g) {
-		g.drawImage(img1, x, y, this);
+		g.drawImage(img1, x-size/2, y-size/2, this);
 	}
 	public boolean mouseDown(Event e, int x, int y) {
 		this.x = x;
